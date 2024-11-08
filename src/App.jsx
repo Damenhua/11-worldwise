8d8dcecc6b415405e5b745d5cd8d1c162ede209e
Damenhua <Damenhua123@gmail.com>
2024-11-08 15:16:15 +0800

    set Routes-1

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import Login from "./pages/Login";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="login" element={<Login />} />
-        <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
