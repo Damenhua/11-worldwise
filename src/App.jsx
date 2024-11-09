a88ed987e775e1a35e8d95fe0aa2e17fd6cff9a7
Damenhua <Damenhua123@gmail.com>
2024-11-09 16:42:00 +0800

    Navigete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import Pricing from "./pages/Pricing";
@@ -7,7 +8,10 @@ import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
+import City from "./components/City";
 import CountryList from "./components/CountryList";
+import Form from "./components/Form";
+
 const BASE_URL = "http://localhost:9000";
 
 function App() {
@@ -38,19 +42,17 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} loading={loading} />}
-          />
+          <Route index element={<Navigate to="cities" replace />} />
           <Route
             path="cities"
             element={<CityList cities={cities} loading={loading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} loading={loading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
