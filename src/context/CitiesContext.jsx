d7680a6daaf25b1d53495a01154a7829d9679e48
Damenhua <Damenhua123@gmail.com>
2024-12-09 14:40:19 +0800

    Cange deploy URL

--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -6,8 +6,8 @@ import {
   useCallback,
 } from "react";
 
-// const BASE_URL = "/.netlify/functions/cities";
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = "/.netlify/functions/cities";
+// const BASE_URL = "http://localhost:9000";
 
 const CitiesContext = createContext();
 
